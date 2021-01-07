78c313bdc5ea5654087e1c6d5aa7eaffcf4d257f
Mehmood <m.hassan.rahi@gmail.com>
2021-01-07 23:15:32 +0500

    * Added company name and title

--- App.js
+++ App.js
@@ -7,22 +7,27 @@ import {
     ImageBackground,
 } from "react-native";
 
-export default function App() {
+const App = () => {
     return (
         <View style={styles.container}>
             <StatusBar barStyle="light-content" />
+
             <View style={styles.header}>
                 <ImageBackground
                     source={require("./assests/images/header.png")}
                     style={styles.imageBackground}
-                />
+                >
+                    <Text style={styles.companyName}>ReactNativeBox</Text>
+                    <Text style={styles.signInText}>Sign in to continue</Text>
+                </ImageBackground>
             </View>
+
             <View style={styles.footer}>
                 <Text>Footer</Text>
             </View>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -44,4 +49,18 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
+
+    companyName: {
+        color: "#fff",
+        fontSize: 25,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+
+    signInText: {
+        color: "yellow",
+        fontSize: 16,
+    },
 });
+
+export default App;
